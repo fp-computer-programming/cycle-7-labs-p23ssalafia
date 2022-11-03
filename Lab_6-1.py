4ec4ea40b6b6250188544a6946d3be680c28a9cf
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-11-03 11:15:15 -0400

    Submission for Lab 6-1

---
+++ Lab_6-1.py
@@ -0,0 +1,23 @@
+#Author: Sean Salafia 11/3/22
+
+#create variable
+from array import array
+
+
+my_list = [1,2,3,4,5]
+
+#Print the number 3 in the list
+my_list[2]
+print(my_list[2])
+
+#Print the length of my list
+x = len(my_list)
+print (x)
+
+#Prints my_list 3 times
+y = my_list * 3
+print (y)
+
+#Print "string" as a list
+Conversion = "string"
+print(list(Conversion))
