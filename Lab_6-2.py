6a2ea46fc482b150ca7129a8571266276f70ceaa
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-11-04 11:40:33 -0400

    Lab 6-2 Submission

---
+++ Lab_6-2.py
@@ -0,0 +1,19 @@
+#Author: Sean Salafia 11/4/22
+
+#set variables, create a list, and replace the 2nd index with the name Sean
+from readline import insert_text
+
+
+my_row = ["Vishnu", "Will"]
+my_row .insert = (2, "Sean")
+
+print(my_row)
+
+#create variables, print 2nd "my row" and delete the first index from my 1st row.
+my_row2 = my_row
+print(my_row2)
+del my_row[1]
+print(my_row)
+print(my_row2)
+
+#Deletes the first index from both my row 1 and 2
