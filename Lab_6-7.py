561359840aa19bc83b798efe816388d222089a59
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-11-10 11:42:27 -0500

    Lab 6-7 submission

---
+++ Lab_6-7.py
@@ -0,0 +1,19 @@
+# Author: Sean Salafia  11.9.22
+
+#user inputs numeric values
+
+a = input("Input a numeric value: ")
+b = input("Input a numeric value: ")
+c = input("Input a numeric value: ")
+
+x = [a, b, c]
+print(x)
+
+#convert inputs to integers
+int(a)
+int(b)
+int(c)
+
+#create a list of the integers being multiplied by 2 and print that list.
+double = [int(a) * 2, int(b) * 2, int(c) * 2]
+print (double)
