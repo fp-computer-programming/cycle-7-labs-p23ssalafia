3988c511e78b5cec922ba266aede4a17e47b91b2
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-11-10 12:11:06 -0500

    Lab 6-5 Resubmission

--- Lab_6-5.py
+++ Lab_6-5.py
@@ -1,19 +1,25 @@
 # Author: Sean Salafia  11.9.22
 
 # set a list equal to x
-x = [ "45", "54", "22"]
 
+
+x = ["23", "20", "10"]
 #check the length of x
 #if the length is greater than 1, execute the code
 #else, print an error message.
+x.sort (reverse = True)
+
 if len(x) > 2:
-    x.sort ()
-    print(x)
-
-    print ("Printed below is the largest number in the sequence:")
-    print (x[-1])
-
-    print ("Printed below is the smallest number for the sequence:")
-    print (x[0])
+      print ("Printed below is the smallest number in the sequence:")
+      print (x[-1])
+      print ("Printed below is the largest number for the sequence:")
+      print (x[0])
 else:
     print("Error! This list is too short")
+
+if x[-1] != x[0]:
+   print("List meets specifications!")
+else:
+   print("Error! List does not meet specification!")
+
+
