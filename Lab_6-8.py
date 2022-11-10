5a6c90f8bc00e288fbadb165847d7d6cbc60ae51
SeanSalafia <p23ssalafia@fairfieldprep.org>
2022-11-10 12:24:55 -0500

    Lab 6-8 submission

---
+++ Lab_6-8.py
@@ -0,0 +1,16 @@
+# Author: Sean Salafia  11.9.22
+
+#user inputs numeric values
+
+a = int(input("Input a numeric value: "))
+b = int(input("Input a numeric value: "))
+c = int(input("Input a numeric value: "))
+
+x = [a, b, c]
+
+if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
+    print("All of your numbers are even numbers!")
+elif a % 2 != 0 and b % 2 != 0 and c % 2 != 0:
+    print("All of your numbers are odd numbers!")
+else:
+    print("Your list of numbers is mixed")
